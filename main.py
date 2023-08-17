428b78b3a7f202874eb03e95e021ccb7d05fd60b
MinyazevR <eeuriset8@gmail.com>
2023-08-17 17:15:30 +0300

    add Sentry

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import more_itertools as mit
 import aiofiles
 import pandas as pd
+import sentry_sdk
 from app.division_handler import process_divisions
 from app.program_handler import process_programs
 from app.groups_handler import process_groups
@@ -20,16 +21,21 @@ async def fill_users() -> None:
 
 
 async def fill_events():
-    now = datetime.datetime.today()
-    snd_date = now + datetime.timedelta(days=28)
+    #now = datetime.datetime.today()
+    #snd_date = now + datetime.timedelta(days=28)
+    now = datetime.datetime.strptime('2023-06-01', '%Y-%m-%d')
+    snd_date = datetime.datetime.strptime('2023-08-01', '%Y-%m-%d')
     urls = await get_events_proxy_urls()
-    user_row, groups = await get_user_and_group_rows(dt_left_date, dt_right_date)
-    counter = process_events(f"{now.year}-{now.month}-{now.day}", f"{snd_date.year}-{snd_date.month}-{snd_date.day}",
+    user_row, groups = await get_user_and_group_rows(now, snd_date)
+    counter = await process_events(f"{now.year}-{now.month}-{now.day}", f"{snd_date.year}-{snd_date.month}-{snd_date.day}",
                              urls, user_row, groups)
     await put_events_proxy_urls(urls, counter)
 
 
 if __name__ == '__main__':
+    sentry_sdk.init(
+        dsn="https://ba2ef865354a9932dbe945a058bb50cf@o4505702880641024.ingest.sentry.io/4505702896173056",
+        traces_sample_rate=0.85)
     if sys.argv[1] == "users":
         asyncio.run(fill_users())
     elif sys.argv[1] == "events":
