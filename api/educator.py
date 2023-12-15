e7d018f4dcf3d8cd1456625a7d648d6c30e9fa51
Anton Kraev <antonkraeww@gmail.com>
2023-12-15 18:38:29 +0300

    implemented endpoints for finding group and educator by name

--- api/educator.py
+++ api/educator.py
@@ -1,17 +1,30 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from api.database import get_db
-from api.schemas import EventListResponse
+from app.models.educator import Educator
+from .database import get_async_session
+from .schemas import EventListResponse
 
 router = APIRouter()
 
 
-@router.head("/find", response_model=int)
-async def educator_id_by_name(first_name: str, last_name: str, middle_name: str, db: Session = Depends(get_db)):
-    ...
+@router.get("/find", response_model=int)
+async def educator_id_by_name(first_name: str, last_name: str, middle_name: str,
+                              session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Educator.id).where((Educator.first_name == first_name)
+                                          & (Educator.last_name == last_name)
+                                          & (Educator.middle_name == middle_name))
+    result = await session.execute(stmt)
+    educator_id = result.scalar_one_or_none()
+
+    if not educator_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No educator with name {first_name} {last_name} {middle_name} found')
+    return educator_id
 
 
 @router.get("/{educator_id}/events", response_model=EventListResponse)
-async def get_educator_events(group_id: int, db: Session = Depends(get_db)):
+async def get_educator_events(group_id: int, session: AsyncSession = Depends(get_async_session)):
     ...
--- api/group.py
+++ api/group.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from api.database import get_db
-from api.schemas import EventListResponse
+from app.models.group import Group
+from .database import get_async_session
+from .schemas import EventListResponse
 
 router = APIRouter()
 
 
-@router.head("/find",  response_model=int)
-def group_id_by_name(name: str, db: Session = Depends(get_db)):
-    ...
+@router.get("/find",  response_model=int)
+async def group_id_by_name(name: str, session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Group.id).where(Group.name == name)
+    result = await session.execute(stmt)
+    group_id = result.scalar_one_or_none()
+
+    if not group_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No group with name {name} found')
+    return group_id
 
 
 @router.get("/{group_id}/events", response_model=EventListResponse)
