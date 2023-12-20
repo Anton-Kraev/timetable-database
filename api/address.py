870ae8f3c2d66ec44e7d2466cb1666c09c2b94b0
Anton Kraev <antonkraeww@gmail.com>
2023-12-20 13:37:30 +0300

    added address and classroom api routers

---
+++ api/address.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.address import Address
+from .database import get_async_session
+from .schemas import AddressListResponse
+
+router = APIRouter()
+
+
+@router.get("/all", response_model=AddressListResponse)
+async def all_addresses(session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Address)
+    result = await session.execute(stmt)
+    addresses = result.scalars().all()
+
+    return AddressListResponse(addresses=addresses)
--- api/main.py
+++ api/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 
 from .group import router as group_router
 from .educator import router as educator_router
+from .address import router as address_router
+from .classroom import router as classroom_router
+
 
 app = FastAPI(title='Timetable API')
 
 app.include_router(group_router, tags=['Group'], prefix='/api/group')
 app.include_router(educator_router, tags=['Educator'], prefix='/api/educator')
+app.include_router(address_router, tags=['Address'], prefix='/api/address')
+app.include_router(classroom_router, tags=['Classroom'], prefix='/api/classroom')
